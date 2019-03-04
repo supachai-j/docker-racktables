385293142b4ad2de1fb53291c0d6a0962dd28f1e
Supachai Jaturaprom <jaturaprom.su@gmail.com>
2019-03-04 23:56:10 +0700

    add init-db

--- Dockerfile
+++ Dockerfile
@@ -12,19 +12,30 @@ RUN \
                      libsnmp-dev \
                      snmp \
                      libldb-dev \
+                     mysql \
   && docker-php-ext-install gd \
                             snmp \
                             pdo_mysql \
                             mcrypt \
                             bcmath \
                             mbstring \
-                            ldap \
+                            #ldap \
                             pcntl \
   && rm -rf /var/lib/apt/lists/*
 
 ## -- Copy File --
 COPY ./racktables /racktables
 
+COPY ./init-db.sql /init-db.sql
+
+RUN \
+  tar -zxf /racktables/RackTables-*.tar.gz -C /tmp && \
+  cd /tmp/RackTables-*/ && make install && find /etc/apache2 -type f -print0 \
+  | xargs -0 sed -i 's@/var/www/html@/usr/local/share/RackTables/wwwroot@g;s@Directory /var/www@Directory /usr/local/share/RackTables@g' \
+  && touch /usr/local/share/RackTables/wwwroot/inc/secret.php \
+  && chmod a=rw '/usr/local/share/RackTables/wwwroot/inc/secret.php' \
+  && chown -R www-data: /usr/local/share/RackTables/wwwroot
+  
 ## --- Start ---
 COPY startup.sh /startup.sh
 RUN dos2unix /startup.sh
--- startup.sh
+++ startup.sh
@@ -2,24 +2,34 @@
 
 if [ ! -f /configured ]; then
 
+# Initial DB Racktables
+# setup database credential settings
+echo "$(date +%F_%R) [New Install] Applying enviromental variables to configurations."
+sed -i -e "s/%DB_NAME%/${DB_NAME}/" \
+       -e "s/%DB_USER%/${DB_USER}/" \
+       -e "s/%DB_PASS%/${DB_PASS}/" \
+    /init-db.sql
+
+echo "$(date +%F_%R) [New Install] Initial Database."
+
 # Installation Racktables
-tar -zxf /racktables/RackTables-*.tar.gz -C /tmp
-cd /tmp/RackTables-*/ && make install && find /etc/apache2 -type f -print0 \
-  | xargs -0 sed -i 's@/var/www/html@/usr/local/share/RackTables/wwwroot@g;s@Directory /var/www@Directory /usr/local/share/RackTables@g' \
-  && touch /usr/local/share/RackTables/wwwroot/inc/secret.php \
-  && chmod 666 /usr/local/share/RackTables/wwwroot/inc/secret.php \
-  && chown -R www-data: /usr/local/share/RackTables/wwwroot
+#tar -zxf /racktables/RackTables-*.tar.gz -C /tmp && \
+#  cd /tmp/RackTables-*/ && make install && find /etc/apache2 -type f -print0 \
+#  | xargs -0 sed -i 's@/var/www/html@/usr/local/share/RackTables/wwwroot@g;s@Directory /var/www@Directory /usr/local/share/RackTables@g' \
+#  && touch /usr/local/share/RackTables/wwwroot/inc/secret.php \
+#  && chmod 666 /usr/local/share/RackTables/wwwroot/inc/secret.php \
+#  && chown -R www-data: /usr/local/share/RackTables/wwwroot
 
 ## set database racktables
-cat <<EOF > /usr/local/share/RackTables/wwwroot/inc/secret.php
-<?php
-\$pdo_dsn = 'mysql:host=$DB_HOST;dbname=$DB_NAME';
-\$db_username = '$DB_USER';
-\$db_password = '$DB_PASS';
+#cat <<EOF > /usr/local/share/RackTables/wwwroot/inc/secret.php
+#<?php
+#\$pdo_dsn = 'mysql:host=$DB_HOST;dbname=$DB_NAME';
+#\$db_username = '$DB_USER';
+#\$db_password = '$DB_PASS';
 #\$user_auth_src = 'database';
 #\$require_local_account = TRUE;
-?>
-EOF
+#?>
+#EOF
 
 # set permission
 chown www-data: /usr/local/share/RackTables/wwwroot/inc/secret.php
